19192d9da0c6098456437e559baa8702a4442cc5
james <james@DESKTOP-CHJTSOM>
2024-06-12 06:47:49 +0100

    Implemented WordFrequency class

---
+++ IntactModule/WordFrequency.cs
@@ -0,0 +1,19 @@
+﻿namespace Test
+{
+    internal class WordFrequency : IWordFrequency
+    {
+        private string _word;
+        private int _frequency;
+
+        public string Word
+        {
+            get => _word;
+            set => _word = value;
+        }
+        public int Frequency
+        {
+            get => _frequency;
+            set => _frequency = value;
+        }
+    }
+}
