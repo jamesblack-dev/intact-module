e9f08790df2b5aadc6ac54b013b0c57e0590bcba
james <james@DESKTOP-CHJTSOM>
2024-06-12 08:56:31 +0100

    test: added test for multi-paragraph

--- IntactModule.UnitTests/WordFrequencyAnalyzerTests.cs
+++ IntactModule.UnitTests/WordFrequencyAnalyzerTests.cs
@@ -11,24 +11,34 @@ namespace IntactModule.UnitTests
             analyzer = new WordFrequencyAnalyzer();
         }
         [Test]
-        public void WordFrequencyAnalyzer_CalculateFrequencyForWord_Success()
+        public void WordFrequencyAnalyzer_CalculateFrequencyForWord_SimpleSentence_Success()
         {
-            var testString = "The dog is the dog";
+            var testString = "The sun shines over the lake";
             var mostFrequentWords = analyzer.CalculateMostFrequentWords(testString, 2);
             Assert.That(mostFrequentWords.Count, Is.EqualTo(2));
-            Assert.IsTrue(string.Compare("dog", mostFrequentWords[0].Word, true) == 0);
-            Assert.IsTrue(string.Compare("the", mostFrequentWords[1].Word, true) == 0);
+            Assert.IsTrue(string.Compare("the", mostFrequentWords[0].Word, true) == 0);
+            Assert.IsTrue(string.Compare("lake", mostFrequentWords[1].Word, true) == 0);
         }
 
         [Test]
-        public void WordFrequencyAnalyzer_CalculateFrequencyForWord_Success2()
+        public void WordFrequencyAnalyzer_CalculateFrequencyForWord_AdditionalPunctuation_Success()
         {
             // Doesn't handle apostrophes
-            var testString = "The dog is the dog you're";
+            var testString = "You're The sun shines over you're the lake you'RE";
             var mostFrequentWords = analyzer.CalculateMostFrequentWords(testString, 2);
             Assert.That(mostFrequentWords.Count, Is.EqualTo(2));
-            Assert.IsTrue(string.Compare("dog", mostFrequentWords[0].Word, true) == 0);
-            Assert.IsTrue(string.Compare("the", mostFrequentWords[1].Word, true) == 0);
+            Assert.IsTrue(string.Compare("re", mostFrequentWords[0].Word, true) == 0);
+            Assert.IsTrue(string.Compare("you", mostFrequentWords[1].Word, true) == 0);
+        }
+
+        [Test]
+        public void WordFrequencyAnalyzer_CalculateFrequencyForWord_MultiSentenceParagraph_Success()
+        {
+            var testString = "The sun shines over the lake. And the sun sets in the evening. Sun.";
+            var mostFrequentWords = analyzer.CalculateMostFrequentWords(testString, 2);
+            Assert.That(mostFrequentWords.Count, Is.EqualTo(2));
+            Assert.IsTrue(string.Compare("the", mostFrequentWords[0].Word, true) == 0);
+            Assert.IsTrue(string.Compare("sun", mostFrequentWords[1].Word, true) == 0);
         }
 
         [Test]
